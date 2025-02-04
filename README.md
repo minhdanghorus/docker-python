2f5b5ba53621c00ead5173ad5ab1d7839d1b89a3
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2025-02-04 22:29:27 +0700

    Docker Compose | How to configure PostgreSQL or MySQL database with Python Django

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # docker-python
 This is a project for learning docker
+
+### Lesson 3: Docker Compose | How to configure PostgreSQL or MySQL database with Python Django
+**url**: https://www.youtube.com/watch?v=q75wgk9jVjA&list=PLOLrQ9Pn6cazCfL7v4CdaykNoWMQymM_C&index=3
+
+**commands**:
+    docker-compose build \
+    docker-compose run --rm app django-admin startproject core . \
+    docker-compose up \
+    docker exec -it docker-python bash \
+    python manage.py migrate \
--- core/settings.py
+++ core/settings.py
@@ -74,13 +74,34 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-app-db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
